7c60eba293a714e0b460e0ae139ebb4e8cb73687
i-p-loginov <ivan.p.loginov@gmail.com>
2015-04-25 15:56:23 +0300

    Revert "Multithread UDP server added, UDP client fixed added"
    This reverts commit 6f5ca819bdcecb0f76dc460b84f5a4185c4390d1.

--- Udp.Sync.Client/Program.cs
+++ Udp.Sync.Client/Program.cs
@@ -5,7 +5,6 @@ using System.Net.Sockets;
 using System.Text;
 using NetworkSamples.Model;
 using System.Net;
-using System.Threading;
 
 namespace Udp.Sync.Client
 {
@@ -22,37 +21,21 @@ namespace Udp.Sync.Client
                 return;
             }
 
-            bool working = true;
-
-            //предполагаем, что аргументы переданы в корректном формате
+             //предполагаем, что аргументы переданы в корректном формате
             var targetIp = IPAddress.Parse(args[0]);
             var port = int.Parse(args[1]);
 
             EndPoint endPoint = new IPEndPoint(targetIp, port);
-            EndPoint srcEndPoint = new IPEndPoint(targetIp, port);
 
             Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            client.Bind(new IPEndPoint(IPAddress.Any, 0));
 
-            Thread receiverWorker = new Thread(() => {
-                while (working)
-                {
-                    var message = client.UdpReceiveMessage(ref srcEndPoint);
-
-                    if (endPoint.ToString() == srcEndPoint.ToString())
-                        Console.WriteLine(message);
-                }
-            });
-
-            receiverWorker.Start();
-
-            while (working)
+            while (true)
             {
                 var message = Console.ReadLine();
 
                 client.UdpSendMessage(endPoint, message);
             }
-
+           
         }
     }
 }
